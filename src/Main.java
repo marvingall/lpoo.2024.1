d414e598bbfaece1bbf45e8d14ef939b6fd20d06
Marvin Gall <144487945+marvingall@users.noreply.github.com>
2024-04-11 12:33:39 -0300

    Delete src directory

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-class Main {
-    public static void main(String[] args) {
-        Carro carro1 = new Carro("Mercedes", "Beinz", "12");
-        Carro carro2 = new Carro("Fiat", "Toro", "8");
-
-        carro1.ligarCarro();
-        carro2.desligarCarro();
-    }
-}
